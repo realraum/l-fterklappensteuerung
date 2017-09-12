608a6769a8f0f7017403d6ac78a9bd88f286062e
Bernhard Tittelbach <bernhard@tittelbach.org>
2017-09-12 03:09:19 +0200

    fix disable blanking/dpms

--- touchpi/aptscript.sh
+++ touchpi/aptscript.sh
@@ -4,4 +4,4 @@ sudo apt-get purge --yes dphys-swapfile
 sudo apt autoremove
 sudo apt-get upgrade --yes
 sudo apt-get install --yes --no-install-recommends lightdm xserver-xorg accountsservice xserver-xorg-video-fbturbo xserver-xorg-input-evdev xinput-calibrator matchbox-keyboard xinput
-sudo apt-get install --yes --no-install-recommends spectrwm unclutter epiphany-browser zsh tmux vim libpam-dbus libpam-systemd rsync
+sudo apt-get install --yes --no-install-recommends spectrwm x11-xserver-utils unclutter epiphany-browser zsh tmux vim libpam-dbus libpam-systemd rsync
