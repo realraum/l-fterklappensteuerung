bc3cb14dfcec1e895775104090ecf8b9573b7e5d
Bernhard Tittelbach <bernhard@tittelbach.org>
2017-09-28 03:07:51 +0200

    no more lightdm

--- touchpi/aptscript.sh
+++ touchpi/aptscript.sh
@@ -3,5 +3,5 @@ sudo apt-get update
 sudo apt-get purge --yes dphys-swapfile avahi-daemon
 sudo apt autoremove
 sudo apt-get upgrade --yes
-sudo apt-get install --yes --no-install-recommends lightdm xserver-xorg accountsservice xserver-xorg-video-fbturbo xserver-xorg-input-evdev xinput-calibrator matchbox-keyboard xinput
+sudo apt-get install --yes --no-install-recommends xserver-xorg xinit accountsservice xserver-xorg-video-fbturbo xserver-xorg-input-evdev xinput-calibrator matchbox-keyboard xinput
 sudo apt-get install --yes --no-install-recommends spectrwm x11-xserver-utils unclutter epiphany-browser zsh tmux vim libpam-dbus libpam-systemd rsync
