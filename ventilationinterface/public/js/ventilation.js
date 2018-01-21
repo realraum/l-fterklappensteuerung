0bcc6a83e8c0a7e0118392d3b80e270c3d765a3c
Bernhard Tittelbach <bernhard@tittelbach.org>
2018-01-21 01:22:22 +0100

    webinterface js needs testing

--- ventilationinterface/public/js/ventilation.js
+++ ventilationinterface/public/js/ventilation.js
@@ -6,7 +6,6 @@ var webSocketSupport = null;
 
 var wsctx_ventchange = "ventchange";
 var wsctx_error = "error";
-var ws_ctx_lock       = "lock";
 var ws_ctx_lock_laser = "locklaser";
 var ws_ctx_lock_olga  = "lockolga";
 
@@ -14,9 +13,11 @@ var auth_token_param_name_ = "authtoken";
 
 function handleExternalControlStateChange(data) {
 	$(".controlstate").removeClass("active");
+	$(".lockbutton").removeClass("active");
 	Object.keys(data).forEach(function(name)  {
 		if (data[name]) {
 			$(".controlstate[name="+name+"][state="+data[name]+"]").addClass("active");
+			$(".lockbutton[name="+name+"]").addClass("active");
 		}
 	});
 }
@@ -33,6 +34,19 @@ function handleControlStateChange(event) {
 	var mygrp = $(event.target).parent().children().removeClass("active");
 	$(event.target).addClass("active");
 	sendControlStateUpdate();
+}
+
+function handleLockButtonClick(event) {
+	//check if our OLGALock would be ignored or not and thus don't display Button Changes that would not reflect reality
+	if ($(event.target).attr("name")!="OLGALock" || getLocationURLParameter(auth_token_param_name_).length==0)
+		return; //only handle OLGALock Button Changes
+	if ($(event.target).hasClass("active"))
+	{
+		$(event.target).removeClass("active");
+	} else {
+		$(event.target).addClass("active");
+	}
+	sendOLGALockUpdate();
 }
 
 function displayErrorMsg(msg, duration) {
@@ -53,11 +67,6 @@ function ShowConnectionEstablished() {
 	$("div.waitingoverlay").css("display","none");
 }
 
-function handleLockStateChange(data) {
-	//TODO
-}
-
-
 function getLocationURLParameter(queryparam) {
 	var urlParams = new URLSearchParams(window.location.search);
 	if (urlParams.has(queryparam))
@@ -67,9 +76,9 @@ function getLocationURLParameter(queryparam) {
 }
 
 function sendOLGALockUpdate() {
-	var newstate=$(".olgalock").hasClass("active");
+	var newstate=$(".lockbutton[name=\"OLGALock\"]").hasClass("active");
 	var local_auth_token=getLocationURLParameter(auth_token_param_name_);
-	ws.send(wsctx_ventchange, {"olga":newstate,"autotoken":local_auth_token});
+	ws.send(ws_ctx_lock_olga, {"olga":newstate,"autotoken":local_auth_token});
 }
 
 (function() {
@@ -78,6 +87,7 @@ function sendOLGALockUpdate() {
   //set background color for fancylightpresetbuttons according to ledr=, ledb=, etc.
   if (webSocketSupport) {
 	$(".controlstate").on("click",handleControlStateChange);
+	$(".lockbutton").on("click",handleLockButtonClick);
 
     ws.registerContext(wsctx_ventchange,handleExternalControlStateChange);
     ws.registerContext(wsctx_error,handleErrorMsg);
