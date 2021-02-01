1074a30bd944c8ee1c954f8c340b303ad9a9d03f
Bernhard Tittelbach <bernhard@tittelbach.org>
2021-02-01 20:32:03 +0100

    on the way to first PCB test

--- firmware/dampercontrol/src/settings.cpp
+++ firmware/dampercontrol/src/settings.cpp
@@ -51,39 +51,39 @@ uint8_t pjon_sensor_destination_id_ = 0; //BROADCAST
 
 void saveSettings2EEPROM()
 {
-  uint8_t *eeprom_pos=0;
-  uint8_t damper_installed=0;
+  // uint8_t *eeprom_pos=0;
+  // uint8_t damper_installed=0;
 
-  eeprom_write_byte(eeprom_pos++, EEPROM_DATA_VERSION);
-  eeprom_write_byte(eeprom_pos++, pjon_device_id_);
-  eeprom_write_byte(eeprom_pos++, NUM_DAMPER);
-  for (uint8_t d=0; d<NUM_DAMPER; d++)
-  {
-    eeprom_write_byte(eeprom_pos++, damper_open_pos_[d]);
-    if (damper_installed_[d])
-      damper_installed |= _BV(d);
-  }
-  eeprom_write_byte(eeprom_pos++, damper_installed);
+  // eeprom_write_byte(eeprom_pos++, EEPROM_DATA_VERSION);
+  // eeprom_write_byte(eeprom_pos++, pjon_device_id_);
+  // eeprom_write_byte(eeprom_pos++, NUM_DAMPER);
+  // for (uint8_t d=0; d<NUM_DAMPER; d++)
+  // {
+  //   eeprom_write_byte(eeprom_pos++, damper_open_pos_[d]);
+  //   if (damper_installed_[d])
+  //     damper_installed |= _BV(d);
+  // }
+  // eeprom_write_byte(eeprom_pos++, damper_installed);
 }
 
 void loadSettingsFromEEPROM()
 {
-  uint8_t *eeprom_pos=0;
+  // uint8_t *eeprom_pos=0;
 
-  if (eeprom_read_byte(eeprom_pos++) != EEPROM_DATA_VERSION)
-    return;
-  pjon_device_id_ = eeprom_read_byte(eeprom_pos++);
-  if (eeprom_read_byte(eeprom_pos++) != NUM_DAMPER)
-    return;
-  for (uint8_t d=0; d<NUM_DAMPER; d++)
-  {
-    damper_open_pos_[d] = eeprom_read_byte(eeprom_pos++);
-  }
-  uint8_t damper_installed = eeprom_read_byte(eeprom_pos++);
-  for (uint8_t d=0; d<NUM_DAMPER; d++)
-  {
-    damper_installed_[d] = 0 < (_BV(d) & damper_installed);
-  }
+  // if (eeprom_read_byte(eeprom_pos++) != EEPROM_DATA_VERSION)
+  //   return;
+  // pjon_device_id_ = eeprom_read_byte(eeprom_pos++);
+  // if (eeprom_read_byte(eeprom_pos++) != NUM_DAMPER)
+  //   return;
+  // for (uint8_t d=0; d<NUM_DAMPER; d++)
+  // {
+  //   damper_open_pos_[d] = eeprom_read_byte(eeprom_pos++);
+  // }
+  // uint8_t damper_installed = eeprom_read_byte(eeprom_pos++);
+  // for (uint8_t d=0; d<NUM_DAMPER; d++)
+  // {
+  //   damper_installed_[d] = 0 < (_BV(d) & damper_installed);
+  // }
 }
 
 void updateSettingsFromPacket(updatesettings_t *s)
